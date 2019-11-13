ca236095a36a7fb062004c676e0d9c72844c594d
Mark McEneaney <markmce30@hotmail.com>
2019-11-13 13:53:57 +0000

    All Rich web until now

---
+++ HTML/Lab 3/Lab 1 stuff/Part 3/lab1p3.js
@@ -0,0 +1,43 @@
+addLi = document.getElementById('addLi')
+Rx.Observable.fromEvent(addLi, 'click')
+    .subscribe(e => addList())
+
+			function remove() {
+			var list = document.getElementById("List");
+			var remVal = document.getElementById('remVal').value;
+			list.removeChild(list.childNodes[remVal]);
+			}
+
+            function addList()
+            {
+
+			var txtVal = document.getElementById('txtVal').value;
+					if (txtVal === '') { alert("Text Field Cannot Be Empty"); return;}
+				
+                    listNode = document.getElementById('List');
+                    liNode = document.createElement("LI");
+                    txtNode = document.createTextNode(txtVal);
+            
+                 liNode.appendChild(txtNode);
+                 listNode.appendChild(liNode);
+            
+		
+            }
+			
+ 			function edit() {
+			var list = document.getElementById("List");
+			var editVal = document.getElementById('editVal').value;
+			list.removeChild(list.childNodes[editVal]);
+			
+			var txtVal = document.getElementById('txtVal').value;
+			if (txtVal === '') { alert("Text Field Cannot Be Empty"); return;}
+				
+            listNode = document.getElementById('List');
+            liNode = document.createElement("LI");
+            txtNode = document.createTextNode(txtVal);
+            
+            liNode.appendChild(txtNode);
+            listNode.appendChild(liNode);
+            
+			}           
+
---
+++ HTML/test.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+var words = 'Who is a mong? "mongs"  ';
+words = words + " It is you!"
+console.log(words)
+var a = 7;
+a += 9; //-= /= *= \
+console.log(a)
+var firstname = 'Bruh';
+var lastletter = firstname[firstname.length - 1]; //i just like this cyode
+console.log(lastletter);
+var myArray = ["Nutt", "McEneaney" , "Colfer"];
+myArray.push("Micky");//PUSH AND
+console.log(myArray)
+
+myArray.shift();//POP
+console.log(myArray);
+myArray.unshift("cyode");
+console.log(myArray);
+
+// FEW FUNCTIONS HAIAAAIAIAI
+function myFunction(){
+  
+}
+
+var numArr = [1,2,3,4];
+console.log(JSON.stringify(numArr));
+console.log(numArr);
+
+return a >b; //give a boolean output so if A is bigger than B its then True
